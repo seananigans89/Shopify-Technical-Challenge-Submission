8b87a06fc4b3964079e83c4f5df3449bbe8ae55f
seanstory <skyloftproductions@Skylofts-iMac.cablelan.net>
2022-05-22 19:19:33 -0700

    fetchItems and createItem API calls are functioning.

--- frontend/shopify_intern_frontend/src/App.js
+++ frontend/shopify_intern_frontend/src/App.js
@@ -1,10 +1,79 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import {Formik, Form, Field} from 'formik'
 
 function App() {
+  const [inventory, setInventory] = useState([])
+  
+
+
+  const items = [1, 2, 3, 4, 5]
+  const listItems = items.map((item, key) => 
+  <li key={key}>{item}</li> 
+  )
+
+  const fetchItems = async() =>{
+    await fetch('http://127.0.0.1:8000/items/', {
+      method: 'GET',
+    }).then(res => res.json()
+    ).then(data => setInventory(data))
+  }
+  useEffect(() => {
+    fetchItems()
+  }, []);
+
+  const createItem = async (itemValues)  => {
+    console.log(itemValues)
+    await fetch('http://127.0.0.1:8000/items/', {
+      method: 'POST',
+      body: itemValues,
+      headers:{
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      }
+  
+    })
+    .then(res => res.json()
+    ).then(data => setInventory(...inventory, data))
+  
+  }
+
   return (
     <div className="App">
       <h1>Inventory Manager</h1>
+      <Formik
+      initialValues={{
+        name: '',
+        quantity: '',
+        weight: '',
       
+      }}
+      onSubmit={async (values) => {
+        createItem(JSON.stringify(values));
+      }}
+    >
+      <Form>
+        <label htmlFor="name">Item Name</label>
+        <Field id="name" name="name" placeholder="" />
+
+        <label htmlFor="quantity">Quantity</label>
+        <Field id="quantity" name="quantity" placeholder="" type="number"/>
+
+        <label htmlFor="weight">Weight</label>
+        <Field
+          id="weight"
+          name="weight"
+          placeholder=""
+          type="number"
+        />
+        <button type="submit">Create Item</button>
+      </Form>
+    </Formik>
+    <div>
+      <ul>
+        {listItems}
+      </ul>
+    </div>
     </div>
   );
 }
--- inventorymanager/inventorymanager/settings.py
+++ inventorymanager/inventorymanager/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'items',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'inventorymanager.urls'
 
